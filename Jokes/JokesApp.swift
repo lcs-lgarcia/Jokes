5fd52552800f6bca3434b7887f16c679d0c72bcc
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-04-14 14:43:32 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Lucas García on 14/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
